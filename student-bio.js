762e091779e697a6e61c3f5d140dd31b6310efcf
JDavis93 <Jordan.davis93@aol.com>
2024-02-13 18:31:49 -0500

    homework

---
+++ student-bio.js
@@ -0,0 +1,15 @@
+var personName = "Jordan";
+var pets = 2;
+var funFact = "I am starting JS today";
+
+console.log("My name is" + personName);
+console.log("I have" + pets + "pets");
+console.log("Fun fact: " + funFact);
+
+personName = "Val";
+pets = 3;
+funFact = "I love cats"
+
+console.log("My name is" + personName);
+console.log("I have" + pets + "pets");
+console.log("Fun fact: " + funFact);
